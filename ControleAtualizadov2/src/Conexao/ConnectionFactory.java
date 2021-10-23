ba5398f15f41216eac79edd8d1678a70c4760925
Paulo <pauloleandrolopezzz@gmail.com>
2021-10-23 03:46:31 -0300

    Adicionando o sistema

---
+++ ControleAtualizadov2/src/Conexao/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+	private String usuario = "covid";
+	private String senha = "Covid2021";
+	private String host = "localhost";
+	private String porta = "3306";
+	private String bd = "db_controle_vacinacao";
+
+	public Connection obtemConexao() {
+		try {
+			Connection c = DriverManager.getConnection("jdbc:mysql://" + host + ":" + porta + "/" + bd, usuario, senha);
+			return c;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
